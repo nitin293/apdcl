b81bd2af571238add708c0ecf7b975196c6b7325
Jintus <jintusharma56@gmail.com>
2021-11-28 17:47:09 +0530

    Change in line13 ......... iloc to loc

--- test1.py
+++ test1.py
@@ -1,12 +1,22 @@
 import pandas as pd
-import numpy as np
 from openpyxl import Workbook
 
-#import the csv file
-df = pd.read_csv('sample.csv')
+
+#taking input from user
+col_name = input("Enter the name of the Columns : ")
+row_s = int(input("Enter the Starting Rows : "))
+row_e = int(input("Enter the Ending Rows : "))
+
+
+#Selecting Specific Rows and Columns
+sel_c = pd.read_csv('sample.csv', usecols = [col_name])
+sel_r = sel_c.loc[row_s:row_e]
+
 
 #convert to excel file
-intern_proj = pd.ExcelWriter('sample1.xlsx')
-df.to_excel(intern_proj, index = False)
+intern_proj = pd.ExcelWriter('sample_excel.xlsx')
+sel_r.to_excel(intern_proj, index = False)
 
+
+#saving the file
 intern_proj.save()
