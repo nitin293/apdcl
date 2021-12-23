f6a19d1f3bc9146c0752560b79b83796861f3a8e
Jintus <jintusharma56@gmail.com>
2021-12-23 10:15:03 +0530

    modified js by manikangkan

--- Project_2/index.js
+++ Project_2/index.js
@@ -1,3 +1,7 @@
+const searchValueFire = document.querySelector(".searchValueFire");
+
+
+
 function Upload() {
     var fileUpload = document.getElementById("fileUpload");
     var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
@@ -17,11 +21,37 @@ function Upload() {
                         }
                     }
                 }
+                const td = table.querySelectorAll('td');
+                console.log(td);
+                td.forEach(item=>{
+                    console.log(item.toString());
+                    item.forEach(ele=>console.log(ele))
+                    searchValueFire.addEventListener("click", (e)=>{
+                        const searchValue = document.querySelector(".searchValue");
+                        e.preventDefault();
+                        console.log(searchValue.value)
+                        const h1 = document.querySelector('h1');
+                        if(searchValue === item.toString()){
+                            h1.innerText = item;
+                        }
+                        else{
+                            h1.innerText = "nahi hoga";
+                        }
+                    })
+                })
+
+               
+
                 var dvCSV = document.getElementById("dvCSV");
                 dvCSV.innerHTML = "";
                 dvCSV.appendChild(table);
+
+
+                // search on the basic of user input
+            
             }
-            reader.readAsText(fileUpload.files[0]);
+            searchValue=document.getElementsByClassName('searchValue');
+            reader.readAsText(fileUpload.files[searchValue]);
         } else {
             alert("This browser does not support HTML5.");
         }
@@ -29,3 +59,4 @@ function Upload() {
         alert("Please upload a valid CSV file.");
     }
 }
+
